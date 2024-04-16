52966408748b8cc46a0481e2a9526ee849a7f7a4
Caio Hamamura <caiohamamura@gmail.com>
2024-04-16 21:54:14 +0000

    Remove dangling functions

--- src/rasterizeHDF.cpp
+++ src/rasterizeHDF.cpp
@@ -10,22 +10,3 @@ using namespace Rcpp;
 // (useful for testing and development). The R code will be automatically
 // run after the compilation.
 //
-
-//' @export
-// [[Rcpp::export]]
-Rcpp::LogicalVector CPL_set_data_dir(Rcpp::CharacterVector data_dir) {
-    if (data_dir.size() != 1)
-        Rcpp::stop("data_dir should be size 1 character vector"); // #nocov
-
-    std::vector<const char*> cstrings{};
-    for(const auto& string : data_dir)
-        cstrings.push_back((Rcpp::as<std::string>(string)).c_str());
-    
-	OSRSetPROJSearchPaths(cstrings.data());
-    
-    Rcout << proj_context_get_database_path(PJ_DEFAULT_CTX);
-	return true;
-}
-/*** R
-
-*/
