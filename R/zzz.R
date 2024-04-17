70bebbeed9aa12daf2f26d0ccee23e60a9e7c86d
caiohamamura <caiohamamura@gmail.com>
2024-04-17 11:42:10 -0400

    * In windows use pkg-config by default and copy proj.db

--- R/zzz.R
+++ R/zzz.R
@@ -5,13 +5,15 @@
 
 #' @useDynLib gdalBindings, .registration=TRUE
 .onLoad <- function(libname, pkgname) {
-  .gdalBindings_CACHE <- new.env(FALSE, parent = globalenv())
-  assign("old.PROJ_LIB", Sys.getenv("PROJ_LIB"), envir = .gdalBindings_CACHE)
   Rcpp::loadModule("gdal_module", TRUE, TRUE)
   InitializeGDAL()
 }
 
 .onAttach <- function(libname, pkgname) {
-  # Setup copied from rgdal package
-  Sys.setenv("PROJ_LIB" = system.file("proj", package = pkgname)[1])
+  if (Sys.getenv("PROJ_LIB") == "") {
+    Sys.setenv("PROJ_LIB" = system.file("proj", package = pkgname)[1])
+  }
+  if (Sys.getenv("GDAL_DATA") == "") {
+    Sys.setenv("GDAL_DATA" = system.file("gdal", package = pkgname)[1])
+  }
 }
