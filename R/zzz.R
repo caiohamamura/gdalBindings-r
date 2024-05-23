0a986d2f6c28ddec4ef1a637cccb537f29545041
caiohamamura <caiohamamura@gmail.com>
2024-05-23 09:56:49 -0400

    * Always copy proj data dir from the same pkg-config datadir it used during installation

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,16 @@
+OLD_PROJ <- Sys.getenv("PROJ_LIB")
+
 .onUnload <- function(libpath) {
   library.dynam.unload("gdalBindings", libpath)
+  Sys.setenv("PROJ_LIB" =  OLD_PROJ)
   invisible()
 }
 
 #' @useDynLib gdalBindings, .registration=TRUE
 .onLoad <- function(libname, pkgname) {
-  Rcpp::loadModule("gdal_module", TRUE, TRUE)
-  InitializeGDAL()
-}
-
-.onAttach <- function(libname, pkgname) {
-  if (Sys.getenv("PROJ_LIB") == "") {
+  if (system.file("proj", package = pkgname)[1] != "") {
     Sys.setenv("PROJ_LIB" = system.file("proj", package = pkgname)[1])
   }
-  if (Sys.getenv("GDAL_DATA") == "") {
-    Sys.setenv("GDAL_DATA" = system.file("gdal", package = pkgname)[1])
-  }
+  Rcpp::loadModule("gdal_module", TRUE, TRUE)
+  InitializeGDAL()
 }
